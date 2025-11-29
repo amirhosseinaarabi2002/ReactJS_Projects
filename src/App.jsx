3b66b8c61a3d3ac93d332776e8d49b09fd2b718b
amirhosseinaarabi2002 <amiraarabisw2002@gmail.com>
2025-11-29 02:09:09 -0800

    react projects : finish login form and register form

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,78 @@
 import { useState } from "react";
 import "./App.css";
-import Button from "./components/Button";
 
 function App() {
-  const [temp, setTemp] = useState(10);
+  const [formType, setFormType] = useState("login");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-  const decreaseTemp = () => {
-    setTemp(temp - 1);
+  const loginHandler = (event) => {
+    event.preventDefault();
+    console.log(`You logged in as ${username} with password ${password}`);
   };
 
-  const increaseTemp = () => {
-    setTemp(temp + 1);
+  const registerHandler = (event) => {
+    event.preventDefault();
+    console.log(`you registered by ${name} , ${email}, ${password}`);
   };
+
   return (
-    <section id="wrapper" className={temp > 15 ? "too-warm" : "too-cold"}>
-      <header></header>
-      <main id="main" className="">
-        <p id="temp">{temp}C</p>
-        <div id="buttons">
-          <Button
-            id="decrease"
-            clickHandler={decreaseTemp}
-            // label={"decrease"}
-          >
-            decrease
-          </Button>
-          <Button
-            id="increase"
-            clickHandler={increaseTemp}
-            // label={"increase"}
-          >
-            increase
-          </Button>
-        </div>
-      </main>
-      <footer></footer>
-      <div id="bg-blur"></div>
-    </section>
+    <div className="login-page">
+      <div className="form">
+        {formType === "login" ? (
+          <form className="login-form" onSubmit={loginHandler}>
+            <input
+              value={username}
+              type="text"
+              placeholder="username"
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
+            />
+            <input
+              value={password}
+              type="password"
+              placeholder="password"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+            <button type="submit">login</button>
+            <p className="message" onClick={() => setFormType("register")}>
+              Not registered? <a href="#">Create an account</a>
+            </p>
+          </form>
+        ) : (
+          <form className="register-form" onSubmit={registerHandler}>
+            <input
+              value={name}
+              type="text"
+              placeholder="name"
+              onChange={(event) => setName(event.target.value)}
+            />
+            <input
+              value={password}
+              type="password"
+              placeholder="password"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <input
+              value={email}
+              type="text"
+              placeholder="email address"
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <button type="submit">create</button>
+            <p className="message" onClick={() => setFormType("login")}>
+              Already registered? <a href="#">Sign In</a>
+            </p>
+          </form>
+        )}
+      </div>
+    </div>
   );
 }
+
 export default App;
