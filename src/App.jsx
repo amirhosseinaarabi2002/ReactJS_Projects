862388310a4db1d3228c8ddab3e534c439bf153a
amirhosseinaarabi2002 <amiraarabisw2002@gmail.com>
2025-12-13 00:10:07 -0800

    react project : search products

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,53 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import SearchInput from "./components/search-input";
+import Header from "./components/header";
+import Products from "./components/products";
+import Footer from "./components/footer";
 
-import fields from "./../data/fields.js";
-import { useState } from "react";
-import Step from "./components/Step/Step.jsx";
+import productsData from "./../data/products.js";
 
-function App() {
-  const [programmingFields] = useState(fields);
-  const [field, setField] = useState("-1");
-  const [steps, setSteps] = useState([]);
+export default function App() {
+  const [searchValue, setSearchValue] = useState("");
+  const [products, setProducts] = useState([...productsData]);
 
-  const findFieldSteps = (newField) => {
-    if (newField !== "-1") {
-      const mainField = programmingFields.find(
-        (field) => field.id === newField
-      );
-
-      setSteps(mainField.technologies);
-    }
+  const clearSearchResult = () => {
+    setSearchValue("");
+    setProducts([...productsData]);
   };
 
+  const handleSearch = () => {
+    const foundedProducts = productsData.filter((product) =>
+      product.title.includes(searchValue)
+    );
+
+    setProducts(foundedProducts);
+  };
+
+  // DeBounce
+  useEffect(() => {
+    const handler = setTimeout(handleSearch, 500);
+
+    return () => clearTimeout(handler);
+  }, [searchValue]);
+
   return (
-    <>
-      <div id="card">
-        <div id="header">
-          <h1 className="title">مسیر سبز - راهنمای مسیر شما</h1>
-        </div>
-        <div id="content">
-          <div>
-            <select
-              id="select-category"
-              value={field}
-              onChange={(event) => {
-                setField(event.target.value);
-                findFieldSteps(event.target.value);
-              }}
-            >
-              <option value="-1">لطفا فیلد مورد نظر را انتخاب نمایید</option>
-              {programmingFields.map((field) => (
-                <option key={field.id} value={field.id}>
-                  {field.title}
-                </option>
-              ))}
-            </select>
-          </div>
+    <div id="content">
+      <Header />
 
-          {field !== "-1" && (
-            <div>
-              <div className="selected-category-show">
-                <span> نقشه اختصاصی شما برای </span>
-                <span id="selected-roadmap-title"> {field} </span>:
-              </div>
-            </div>
-          )}
+      <SearchInput
+        inputValue={searchValue}
+        onChange={(value) => {
+          setSearchValue(value);
+        }}
+      />
 
-          {/* <!-- is-empty className --> */}
-          <div id="roadmap" className={field === "-1" ? "is-empty" : ""}>
-            {field === "-1" ? (
-              <div className="empty-list">
-                <p>در ابتدا لطفا حوزه مورد نظر خود را انتخاب فرمایید</p>
-              </div>
-            ) : (
-              steps.map((step) => <Step key={step.id} {...step} />)
-            )}
-          </div>
-        </div>
-      </div>
+      <Products
+        products={products}
+        isSearchedSomething={String(searchValue).length}
+        onClearSearchResult={clearSearchResult}
+      />
 
-      <div id="background"></div>
-    </>
+      <Footer />
+    </div>
   );
 }
-
-export default App;
